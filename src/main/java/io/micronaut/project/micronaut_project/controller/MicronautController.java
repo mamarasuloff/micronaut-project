6bf93d96cdc3d2a5fa9b356addc4b052da0ba02a
Zukhriddin Mamarasulov <Zukhriddin.Mamarasulov@kdb.uz>
2025-01-09 18:02:24 +0500

    Update repo

--- src/main/java/io/micronaut/project/micronaut_project/controller/MicronautController.java
+++ src/main/java/io/micronaut/project/micronaut_project/controller/MicronautController.java
@@ -8,17 +8,12 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Header;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.Produces;
-import io.micronaut.http.server.util.HttpHostResolver;
-import io.micronaut.http.server.util.locale.HttpLocaleResolver;
 import io.micronaut.project.micronaut_project.entity.Products;
 import io.micronaut.project.micronaut_project.repository.ProductRepository;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.authentication.Authentication;
-import io.micronaut.security.authentication.AuthenticationResponse;
 import io.micronaut.security.authentication.Authenticator;
 import io.micronaut.security.authentication.UsernamePasswordCredentials;
-import io.micronaut.security.event.LoginFailedEvent;
-import io.micronaut.security.event.LoginSuccessfulEvent;
 import io.micronaut.security.handlers.LoginHandler;
 import io.micronaut.security.rules.SecurityRule;
 import jakarta.inject.Inject;
@@ -33,13 +28,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.micronaut.context.annotation.Requires;
-import io.micronaut.context.event.ApplicationEventPublisher;
 import io.micronaut.core.async.annotation.SingleResult;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 
-@Requires(classes = Controller.class)
 @Requires(beans = {LoginHandler.class, Authenticator.class})
 @Controller("/head")
 public class MicronautController {
@@ -52,15 +45,6 @@ public class MicronautController {
     Authenticator<HttpRequest<?>> authenticator;
     @Inject
     LoginHandler<HttpRequest<?>, MutableHttpResponse<?>> loginHandler;
-    @Inject
-    ApplicationEventPublisher<LoginSuccessfulEvent> loginSuccessfulEventPublisher;
-    @Inject
-    ApplicationEventPublisher<LoginFailedEvent> loginFailedEventPublisher;
-    @Inject
-    HttpHostResolver httpHostResolver;
-    @Inject
-    HttpLocaleResolver httpLocaleResolver;
-    
     
 	@Post("/products")
 	@Produces(MediaType.APPLICATION_JSON) 
@@ -79,9 +63,9 @@ public class MicronautController {
 		return "Test";
 	}
 	
-    @Consumes({MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_JSON})
     @Secured(SecurityRule.IS_ANONYMOUS)
     @Post("/login")
+    @Produces(MediaType.APPLICATION_JSON)
     @SingleResult
     public Publisher<MutableHttpResponse<?>> login(
     		@Valid 
@@ -91,40 +75,15 @@ public class MicronautController {
     	) {
         return Flux.from(authenticator.authenticate(request, usernamePasswordCredentials))
             .map(authenticationResponse -> {
-                if (authenticationResponse.isAuthenticated() && authenticationResponse.getAuthentication().isPresent()) {
+                if(authenticationResponse.isAuthenticated() && authenticationResponse.getAuthentication().isPresent()) {
                     Authentication authentication = authenticationResponse.getAuthentication().get();
-                    publishLoginSuccessfulEvent(authentication, request);
                     return loginHandler.loginSuccess(authentication, request);
                 } else {
                     if (LOG.isTraceEnabled()) {
                         LOG.trace("login failed for username: {}", usernamePasswordCredentials.getUsername());
                     }
-                    publishLoginFailedEvent(authenticationResponse, usernamePasswordCredentials, request);
                     return loginHandler.loginFailed(authenticationResponse, request);
                 }
             }).switchIfEmpty(Mono.defer(() -> Mono.just(HttpResponse.status(HttpStatus.UNAUTHORIZED))));
-    }
-
-    private void publishLoginSuccessfulEvent(Authentication authentication, HttpRequest<?> request) {
-        loginSuccessfulEventPublisher.publishEvent(
-            new LoginSuccessfulEvent(
-                authentication,
-                httpHostResolver.resolve(request),
-                httpLocaleResolver.resolveOrDefault(request)
-            )
-        );
-    }
-
-    private void publishLoginFailedEvent(AuthenticationResponse authenticationResponse,
-                                         UsernamePasswordCredentials usernamePasswordCredentials,
-                                         HttpRequest<?> request) {
-        loginFailedEventPublisher.publishEvent(
-                new LoginFailedEvent(
-                        authenticationResponse,
-                        usernamePasswordCredentials,
-                        httpHostResolver.resolve(request),
-                        httpLocaleResolver.resolveOrDefault(request)
-                )
-        );
     }
 }
